dc9727e87c80e9a415efe2dc81741bc2f13e8688
JCoMcL <rapcyss@gmail.com>
2022-03-14 00:48:01 +0000

    implement most of the work ontop of blocking queues

--- Assignment1.java
+++ Assignment1.java
@@ -7,18 +7,24 @@ import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
 class OrderCounter {
     static AtomicInteger i = new AtomicInteger(0);
-    public static int getID() {
+    static AtomicInteger active = new AtomicInteger(0);
+    public synchronized static int getID() {
+        active.incrementAndGet();
         return i.incrementAndGet();
     }
     public static void release() {
-        if (i.decrementAndGet() == 0)
+        if (active.decrementAndGet() == 0)
             System.exit(0);
+        Assignment1.logger.log(active.get() + " orders active");
     }
 }
 
@@ -43,14 +49,33 @@ class Logger {
         System.setOut(this.cmd);
         System.out.println(s);
     }
+
+    public void die(Exception deathRattle) {
+        System.err.println(deathRattle);
+        System.exit(1);
+    }
 }
 
 class Station extends LinkedBlockingQueue<Vehicle> implements Runnable {
-    private void doWork(Vehicle v) {
+    String name;
+    long threadNumber;
+
+    Station(int capacity, String name) {
+        super(capacity);
+        this.name = name;
+    }
+
+    protected void doWork(Vehicle v) {
+    }
+
+    void log(String message) {
+        Assignment1.logger.log(message);
     }
 
     @Override
     public void run() {
+        this.threadNumber = Thread.currentThread().getId();
+        log(name + ": Active");
         while (true)
         {
             try {
@@ -61,13 +86,23 @@ class Station extends LinkedBlockingQueue<Vehicle> implements Runnable {
     }
 }
 
+class Carrier extends Station {
+    Carrier() {
+        super(6, "Carrier");
+    }
+    protected void doWork(Vehicle v) {
+        OrderCounter.release();
+    }
+}
+
 public class Assignment1 {
     static long randomSeed = 0;
-    Logger logger = new Logger();
+    static Logger logger = new Logger();
+    static AtomicInteger threadCounter = new AtomicInteger(0);
 
     AtomicBoolean continueOrders = new AtomicBoolean(true); // Controls ordering loop
-    Integer millisecondsPerTic = 5;
-    Integer programDuration = 250;
+    static int millisecondsPerTic = 5;
+    static int programDuration = 250;
     Integer orderInterval = 10;
 
     // Maximum capacity per car model
@@ -92,35 +127,35 @@ public class Assignment1 {
     public static void main(String args[]) {
         // Initialisation, main code here
         Assignment1 assignment1 = new Assignment1();
-        Queue<Vehicle> carrier_trailer = new LinkedList<Vehicle>();
+        var carrier_trailer = new Carrier();
 
         // Create Robots for production line
-        List<Robot> robots = new ArrayList<Robot>();
+        var robots = new ArrayList<Station>();
+        robots.add(carrier_trailer);
 
         // 5
-        Robot painter = new Robot("Painter", "Trailer", 20, assignment1, carrier_trailer);
+        Robot painter = new Robot("Painter", 20, carrier_trailer);
         robots.add(painter);
         // 4
-        Robot electrodipper = new Robot("Electrodipper", "Painting", 40, assignment1, painter.waiting_vehicles);
+        Robot electrodipper = new Robot("Electrodipper", 40, painter);
         robots.add(electrodipper);
         // 3
-        Robot primer = new Robot("Primer", "Electrodipping", 30, assignment1, electrodipper.waiting_vehicles);
+        Robot primer = new Robot("Primer", 30, electrodipper);
         robots.add(primer);
         // 2
-        Robot washer = new Robot("Washer", "Priming", 10, assignment1, primer.waiting_vehicles);
+        Robot washer = new Robot("Washer", 10, primer);
         robots.add(washer);
         // 1
-        Robot sander = new Robot("Sander", "Washing", 5, assignment1, washer.waiting_vehicles);
+        Robot sander = new Robot("Sander", 5, washer);
         robots.add(sander);
 
-        // Starting up robots for the production line
-        ExecutorService workerExecutor = Executors.newFixedThreadPool(6);
+        ExecutorService workerExecutor = Executors.newFixedThreadPool(16);
         robots.forEach(robot -> workerExecutor.execute(robot));
 
         // Creating a warehouse object, this handles various functionality related to accepting orders (...)
         // (...) from the dealership, as well as passing cars to/from robots on the production line.
         Warehouse warehouse = new Warehouse(
-                sander.waiting_vehicles,
+                sander,
                 carrier_trailer,
                 assignment1);
 
@@ -134,7 +169,7 @@ public class Assignment1 {
                 timer.cancel();
             }
         };
-        timer.schedule(task, assignment1.millisecondsPerTic * assignment1.programDuration);
+        timer.schedule(task, millisecondsPerTic * programDuration);
 
         // Dealership ordering vehicles starts here
         Dealership dealership = new Dealership(assignment1, warehouse);
@@ -162,15 +197,13 @@ class Warehouse {
     // tics between new cars arriving
     Dictionary<String, AtomicInteger> current_stock;
     Integer limit_per_model;
-    private Queue<Vehicle> placed_orders;
+    private Station placed_orders;
     Queue<Vehicle> carrier_trailer;
     AtomicInteger trailer_counter;
     List<String> car_models;
     Assignment1 assignment1;
 
-    Warehouse(Queue<Vehicle> first_robot_queue,
-              Queue<Vehicle> trailer,
-              Assignment1 assignment1) {
+    Warehouse(Station first_robot_queue, Queue<Vehicle> trailer, Assignment1 assignment1) {
         this.current_stock = new Hashtable<String, AtomicInteger>();
         assignment1.car_models.forEach(car -> current_stock.put(car, new AtomicInteger(5)));
 
@@ -183,9 +216,6 @@ class Warehouse {
     }
 
     public synchronized void placeOrder(List<String> car_models, List<String> colours){
-        // Model/Colour are randomised here, but if you wanted to, could have randomisation/selection done in (...)
-        // (...) the dealership too. It's functionally equivalent, leads to the same result, it's just easier to (...)
-        // (...) work with it this way because of the way I initially planned out these classes.
         Random rng = new Random(Assignment1.randomSeed);
         String random_model = car_models.get(rng.nextInt(car_models.size()));
         String random_colour = colours.get(rng.nextInt(colours.size()));
@@ -195,26 +225,26 @@ class Warehouse {
         if (current_stock.get(vehicle.model).get() > 0) {
             current_stock.get(vehicle.model).decrementAndGet();
 
-            // Redirecting output so logs go out to console as well as the file.
             String outp = ("Received order for vehicle model " + vehicle.model + ", colour " + vehicle.colour + " => There are " + current_stock.get(vehicle.model).get() + " of this model left in stock.");
-            assignment1.logger.log(outp);
+            Assignment1.logger.log(outp);
 
-            synchronized (placed_orders)
-            { placed_orders.add(vehicle);
-            placed_orders.notify(); }
+            try {
+                placed_orders.put(vehicle);
+            } catch(InterruptedException e) {}
         }
     }
     public synchronized void checkCarrierTrailer () {
         if (carrier_trailer.size() >= 3) {
-            synchronized (carrier_trailer)
-            { carrier_trailer.clear();
-            carrier_trailer.notify(); }
+            synchronized (carrier_trailer) {
+                carrier_trailer.clear();
+                carrier_trailer.notify();
+            }
 
             String outp = ("Car carrier trailer departed.");
             assignment1.logger.log(outp);
         }
     }
-    public synchronized void  addToWarehouse() {
+    public synchronized void addToWarehouse() {
         Random rng = new Random(Assignment1.randomSeed);
         String random_model = car_models.get(rng.nextInt(car_models.size()));
         AtomicInteger currentStock = current_stock.get(random_model);
@@ -239,65 +269,33 @@ class Warehouse {
 //    }
 }
 
-class Robot implements Runnable {
-    // Role example: Washing
-    String role;
-    // Status example: Active
-    String status;
-    // Which robot the vehicle being worked on is supposed to be sent to
-    String sendsTo;
-    Integer curr_veh_uid;
+class Robot extends Station {
     Integer job_length_tics;
-    Queue<Vehicle> waiting_vehicles;
-    AtomicBoolean continueOrders;
-    Assignment1 assignment1;
-    Queue<Vehicle> nextStation;
+    Station next;
 
-    Robot(String role, String destination, Integer job_length_tics, Assignment1 assignment1, Queue<Vehicle> nextStation) {
-        super();
-        this.role = role;
-        this.status = "Ready";
-        this.sendsTo = destination;
-        this.curr_veh_uid = -1;
+    Robot(String role, Integer job_length_tics, Station next) {
+        super(1, role);
         this.job_length_tics = job_length_tics;
-        this.waiting_vehicles = new LinkedList<Vehicle>();
-        this.continueOrders = assignment1.continueOrders;
-        this.assignment1 = assignment1;
-        this.nextStation = nextStation;
+        this.next = next;
     }
 
-    @Override
-    public void run() {
-        Vehicle currentVehicle;
-        String outp = ("Robot " + role + " started.");
-        assignment1.logger.log(outp);
+    protected void doWork(Vehicle v) {
+        log("- Thread - " + threadNumber + ": " + "The " + name + " robot is now working on order " + v.uid);
 
-        while (true)
-        {
-            currentVehicle = waiting_vehicles.take();
+        try { Thread.sleep(Assignment1.millisecondsPerTic * job_length_tics); }
+        catch (InterruptedException e) {}
 
-            outp = ("- Thread - " + this.threadNumber + ": " + "The " + role + " robot is now working on order " + currentVehicle.uid);
-            assignment1.logger.log(outp);
+        v.status = name;
+        log("- Thread - " + threadNumber + ": " + name + " is done with vehicle " + v.uid + ". moving to next stage: " + next.name + ".");
 
-            try { Thread.sleep(assignment1.millisecondsPerTic * job_length_tics); }
-            catch (InterruptedException e) {}
-
-            // Once work is completed, move the car to the next queue.
-            currentVehicle.status = role;
-            outp = ("- Thread - " + this.threadNumber + ": " + role + " is done with vehicle " + currentVehicle.uid + ". moving to next stage: " + sendsTo + ".");
-            assignment1.logger.log(outp);
-
-            // Car moving
-            try {Thread.sleep(100);}
-            catch (InterruptedException e) {}
-            synchronized (nextStation)
-            {
-                nextStation.add(currentVehicle);
-                nextStation.notify();
-            }
+        // Car moving
+        try {
+            next.put(v);
         }
+        catch (InterruptedException e) {}
     }
 }
+
 class Dealership implements Runnable{
     AtomicBoolean continueOrders;
     Warehouse warehouse;
